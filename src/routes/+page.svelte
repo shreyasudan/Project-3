10e591200b8d9bd29235eb2bd57ec5a449027650
rharia <102628981+rharia@users.noreply.github.com>
2024-02-18 23:00:43 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,25 @@
 <h1>Welcome to SvelteKit!!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<script>
+
+    import * as d3 from 'd3';
+    
+    import { onMount } from 'svelte';
+
+    let energyData = [];
+
+    onMount(async () => {
+
+        const res = await fetch('raw.githubusercontent.com_owid_energy-data_master_owid-energy-data.csv'); 
+
+        const csv = await res.text();
+
+        energyData = d3.csvParse(csv, d3.autoType)
+
+        console.log(energyData);
+
+    });
+
+
+</script>
